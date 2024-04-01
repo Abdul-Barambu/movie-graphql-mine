56960daaa4fc4db368a3d394964a1f348f56a223
Abdul-Barambu <abdulkadirbarambu1@gmail.com>
2024-04-01 14:43:48 +0100

    first commit

---
+++ movie-graphql-mine/src/main/java/com/abdul/moviegraphqlmine/MovieGraphqlMineApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.moviegraphqlmine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGraphqlMineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieGraphqlMineApplication.class, args);
+	}
+
+}
